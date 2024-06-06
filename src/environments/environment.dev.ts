35c0bcf5b70c36318b84246b0f48da59044d2319
prashant <ppo.opash@gmail.com>
2024-06-06 11:23:34 +0530

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://dev-api.pussy.ventures/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://dev-api.pussy.ventures/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+};
