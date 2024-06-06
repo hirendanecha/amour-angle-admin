0c5d3b493cf4e015844416850093c438d49f0f54
prashant <ppo.opash@gmail.com>
2024-06-06 16:26:07 +0530

    change wordings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'https://api.amourangles.tube/api/v1/',
+  serverUrl: 'https://api.amourangels.tube/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.amourangles.tube/',
+  socketUrl: 'https://api.amourangels.tube/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: 'https://amourangles-api.opash.in/api/v1/',
+  serverUrl: 'https://amourangels-api.opash.in/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://amourangles-api.opash.in/',
+  socketUrl: 'https://amourangels-api.opash.in/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 };
